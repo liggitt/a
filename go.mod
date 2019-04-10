c19534d2bacc81578d1aaa4165ecd3569174cb46
Jordan Liggitt <liggitt@google.com>
2019-04-10 15:58:38 -0400

    require github.com/liggitt/c@v1.0.0

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/liggitt/a
 
 go 1.12
 
-require github.com/liggitt/b v1.0.0
+require (
+	github.com/liggitt/b v1.0.0
+	github.com/liggitt/c v1.0.0
+)
--- make-cake.go
+++ make-cake.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/liggitt/a/helpers"
 	"github.com/liggitt/b"
+	"github.com/liggitt/c"
 )
 
 func main() {
 	b.Cake()
+	fmt.Println(c.CakeOrDeath())
 	helpers.PrintSuccess()
 }
