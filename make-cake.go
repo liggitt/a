cdf5ffc4f2ce0c91af8fcc3a96aae6022d263e28
Jordan Liggitt <liggitt@google.com>
2019-04-10 15:59:54 -0400

    require github.com/liggitt/d v1.0.0, pin github.com/liggitt/c v1.0.0

--- make-cake.go
+++ make-cake.go
@@ -5,10 +5,12 @@ import (
 	"github.com/liggitt/a/helpers"
 	"github.com/liggitt/b"
 	"github.com/liggitt/c"
+	"github.com/liggitt/d"
 )
 
 func main() {
 	b.Cake()
 	fmt.Println(c.CakeOrDeath())
+	fmt.Println(d.Ingredients())
 	helpers.PrintSuccess()
 }
